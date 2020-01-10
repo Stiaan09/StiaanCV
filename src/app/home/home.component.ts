d3fa5b57a333e9a89543a428be84faac1a5dc9e5
Adriaan <ahpienaar@gmail.com>
2020-01-10 18:58:54 +0200

    Add home

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  public cat: string = "https://icatcare.org/app/uploads/2018/09/Scottish-fold.png"
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
