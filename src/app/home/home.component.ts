4a8c1436006f74558f9774db8b4b28c63e14681e
Ruan Kruger <ruan@responsive.co.za>
2020-01-14 14:17:23 +0200

    Navbar sticky and Styling
    - Updated NavBar to be sticky
    - Moved inline styling to css file
    - Commented on what to do next

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,21 +7,21 @@ import { Component, OnInit } from '@angular/core';
 
 })
 export class HomeComponent implements OnInit {
-  public cat: string = "https://icatcare.org/app/uploads/2018/09/Scottish-fold.png";
-  public Piesang: string = "https://media-exp2.licdn.com/dms/image/C4D03AQHfxqSb9Z-gjw/profile-displayphoto-shrink_200_200/0?e=1584576000&v=beta&t=Hc9kgcDnGANxq6huiiBfE7mruRBOAUD188QJcFZoAec";
   constructor() { }
 
   ngOnInit() {
+    window.onscroll = function() {myFunction()};
+  
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+    function myFunction() {
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+      } else {
+        navbar.classList.remove("sticky");
+      }
+    }
   }
 
   Stiaan:string="assets/Images/PFP.jpg";
-
-
-
 }
-// @Component({
-//   selector:'menu-overview-example',
-//   templateUrl:'menu-overview-example.html',
-//   styleUrls:['menu-overview-example.css'],
-// })
-// export class MenuOverviewExample{}
